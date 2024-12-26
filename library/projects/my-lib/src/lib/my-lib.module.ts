68245e5f2e3feb04c17c444ebb4c03c5030078d8
UAB\khantwinsoe <khantwinsoe@uab.com.mm>
2024-12-26 17:18:38 +0630

    feat: create new lib component

--- library/projects/my-lib/src/lib/my-lib.module.ts
+++ library/projects/my-lib/src/lib/my-lib.module.ts
@@ -1,29 +1,39 @@
 import { NgModule } from '@angular/core';
-import { MyLibComponent } from './my-lib.component';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MyLibComponent } from './my-lib.component';
+import { AlertBoxComponent } from './components/alert-box/alert-box.component';
+import { ButtonComponent } from './components/button/button.component';
+import { DataGridComponent } from './components/data-grid/data-grid.component';
+import { DropdownBoxComponent } from './components/dropdown-box/dropdown-box.component';
+import { FileUploadPreviewComponent } from './components/file-upload-preview/file-upload-preview.component';
 import { InputBoxComponent } from './components/input-box/input-box.component';
 import { NrcBoxComponent } from './components/nrc-box/nrc-box.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { DropdownBoxComponent } from './components/dropdown-box/dropdown-box.component';
 import { RadioBoxComponent } from './components/radio-box/radio-box.component';
-import { AlertBoxComponent } from './components/alert-box/alert-box.component';
 
 @NgModule({
   declarations: [
     MyLibComponent,
+    AlertBoxComponent,
+    ButtonComponent,
+    DataGridComponent,
+    DropdownBoxComponent,
+    FileUploadPreviewComponent,
     InputBoxComponent,
     NrcBoxComponent,
-    DropdownBoxComponent,
     RadioBoxComponent,
-    AlertBoxComponent,
   ],
   imports: [CommonModule, FormsModule, ReactiveFormsModule, BrowserModule],
   exports: [
     MyLibComponent,
+    AlertBoxComponent,
+    ButtonComponent,
+    DataGridComponent,
+    DropdownBoxComponent,
+    FileUploadPreviewComponent,
     InputBoxComponent,
     NrcBoxComponent,
-    DropdownBoxComponent,
     RadioBoxComponent,
   ],
   bootstrap: [NrcBoxComponent],
