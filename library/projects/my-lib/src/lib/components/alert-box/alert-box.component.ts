0b08827117136ed42547f0277f007a451776ca4b
UAB\khantwinsoe <khantwinsoe@uab.com.mm>
2024-12-27 15:46:24 +0630

    feat: library global scss variable update

--- library/projects/my-lib/src/lib/components/alert-box/alert-box.component.ts
+++ library/projects/my-lib/src/lib/components/alert-box/alert-box.component.ts
@@ -3,10 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'lib-alert-box',
   standalone: false,
-  
   templateUrl: './alert-box.component.html',
-  styleUrl: './alert-box.component.css'
+  styleUrl: './alert-box.component.scss',
 })
-export class AlertBoxComponent {
-
-}
+export class AlertBoxComponent {}
